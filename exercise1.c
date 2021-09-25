f3e1dcf45afd491e2a0c5b4493a9bad715b03265
lekipising <leeplanke@gmail.com>
2021-09-25 11:23:39 +0200

    exercise one

--- exercise1.c
+++ exercise1.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void oddEven(int up)
+{
+    int bound = up;
+    int sumOfOdd = 0;
+    int sumOfEven = 0;
+    int difOddEven;
+    double thirdEven;
+    double fifthOdd;
+
+    while (bound > 0)
+    {
+        //even
+        if (bound % 2 == 0)
+        {
+            sumOfEven += bound;
+        }
+        //odd
+        else
+        {
+            sumOfOdd += bound;
+        }
+
+        bound--;
+    }
+
+    printf("The sum of even numbers is : %d \n", sumOfEven);
+    printf("The sum of odd numbers is : %d \n", sumOfOdd);
+    printf("The absolute difference of sum of odd and sum of even is : %d \n", abs(sumOfOdd - sumOfEven));
+    printf("A third of the sum of even numbers is : %lf \n", 0.3333 * sumOfEven);
+    printf("A third of the sum of odd numbers is : %lf \n", 0.2 * sumOfOdd);
+}
 
 int main()
 {
-
+    int upperBound;
+    printf("Enter the upper bound : ");
+    scanf("%d", &upperBound);
+    oddEven(upperBound);
     return 0;
 }
